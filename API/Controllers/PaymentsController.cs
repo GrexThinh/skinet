56313c58e73ac2a5153ee6d762ad6f7cecee097d
GrexThinh <vanthinhnq@gmail.com>
2023-07-14 11:19:26 +0700

    fix

--- API/Controllers/PaymentsController.cs
+++ API/Controllers/PaymentsController.cs
@@ -16,64 +16,64 @@ using Order = Core.Entities.OrderAggregate.Order;
 
 namespace API.Controllers
 {
-    public class PaymentsController : BaseApiController
-    {
-        private readonly string WhSecret = "";
-        private readonly IPaymentService _paymentService;
-        private readonly ILogger<PaymentsController> _logger;
+    // public class PaymentsController : BaseApiController
+    // {
+    //     private readonly string WhSecret = "";
+    //     private readonly IPaymentService _paymentService;
+    //     private readonly ILogger<PaymentsController> _logger;
 
-        public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger, IConfiguration config)
-        {
-            _paymentService = paymentService;
-            _logger = logger;
-            WhSecret = config.GetSection("StripeSettings:WhSecret").Value;
-        }
+    //     public PaymentsController(IPaymentService paymentService, ILogger<PaymentsController> logger, IConfiguration config)
+    //     {
+    //         _paymentService = paymentService;
+    //         _logger = logger;
+    //         WhSecret = config.GetSection("StripeSettings:WhSecret").Value;
+    //     }
 
-        [Authorize]
-        [HttpPost("{basketId}")]
-        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
-        {
-            var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+    //     [Authorize]
+    //     [HttpPost("{basketId}")]
+    //     public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+    //     {
+    //         var basket = await _paymentService.CreateOrUpdatePaymentIntent(basketId);
 
-            if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
+    //         if (basket == null) return BadRequest(new ApiResponse(400, "Problem with your basket"));
 
-            return basket;
-        }
+    //         return basket;
+    //     }
 
-        [HttpPost("webhook")]
-        public async Task<ActionResult> StripeWebhook()
-        {
-            var json = await new StreamReader(Request.Body).ReadToEndAsync();
+    //     [HttpPost("webhook")]
+    //     public async Task<ActionResult> StripeWebhook()
+    //     {
+    //         var json = await new StreamReader(Request.Body).ReadToEndAsync();
 
-            var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
+    //         var stripeEvent = EventUtility.ConstructEvent(json, Request.Headers["Stripe-Signature"], WhSecret);
 
-            PaymentIntent intent;
-            Order order;
+    //         PaymentIntent intent;
+    //         Order order;
 
-            switch(stripeEvent.Type)
-            {
-                case "payment_intent.succeeded":
-                    intent = (PaymentIntent)stripeEvent.Data.Object;
-                    _logger.LogInformation("Payment Succeeded: ", intent.Id);
-                    // update order with new status
-                    order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
-                    _logger.LogInformation("Order updated to payment received: ", order.Id);
-                    break;
+    //         switch(stripeEvent.Type)
+    //         {
+    //             case "payment_intent.succeeded":
+    //                 intent = (PaymentIntent)stripeEvent.Data.Object;
+    //                 _logger.LogInformation("Payment Succeeded: ", intent.Id);
+    //                 // update order with new status
+    //                 order = await _paymentService.UpdateOrderPaymentSucceeded(intent.Id);
+    //                 _logger.LogInformation("Order updated to payment received: ", order.Id);
+    //                 break;
 
-                case "payment_intent.payment_failed":
-                    intent = (PaymentIntent)stripeEvent.Data.Object;
-                    _logger.LogInformation("Payment Failed: ", intent.Id);
-                    // update order status
-                    order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
-                    _logger.LogInformation("Payment Failed: ", order.Id);
+    //             case "payment_intent.payment_failed":
+    //                 intent = (PaymentIntent)stripeEvent.Data.Object;
+    //                 _logger.LogInformation("Payment Failed: ", intent.Id);
+    //                 // update order status
+    //                 order = await _paymentService.UpdateOrderPaymentFailed(intent.Id);
+    //                 _logger.LogInformation("Payment Failed: ", order.Id);
 
-                    break;
+    //                 break;
 
-                default:
-                    break;
-            }
+    //             default:
+    //                 break;
+    //         }
 
-            return new EmptyResult();
-        }
-    }
+    //         return new EmptyResult();
+    //     }
+    // }
 }
